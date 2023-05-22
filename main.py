9c6bed5761e1bc35de12ba07fe89c42835bc4ac9
Mario <mariojunior200218@gmail.com>
2023-05-23 00:20:50 +0100

    bot implemented with openAI

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from slack import WebClient
 from slack.errors import SlackApiError
 from datetime import datetime
 from data import check_birthdays
-# from data import my_daily_task
+# from data import users_list
 
-# users_list()
-check_birthdays()
+# users_list() 
+# channels_list()
 
 # schedule the daily task
 schedule.every().day.at("10:30").do(check_birthdays)
-
 
 # while loop to keep the scheduler running
 while True:
